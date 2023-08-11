2437a34fc0e5e1193288b792283ea5fdab8bf671
KorianderKAemulus <114248547+KorianderK@users.noreply.github.com>
2023-08-12 01:06:33 +0800

    Commit 3: Front and Backend work
    Connected React front end with Flask backend

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3", "uwu"]}
+    return {"members": ["Aidan", "Amos", "Zafri", "Raveendran"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
