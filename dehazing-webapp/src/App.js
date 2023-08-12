c3e8288fa1d4cc00c264f4e6efd806e70f498a46
KorianderKAemulus <114248547+KorianderK@users.noreply.github.com>
2023-08-13 00:52:15 +0800

    Commit 4: Removed previous backend code to focus on image input and resizing

--- dehazing-webapp/src/App.js
+++ dehazing-webapp/src/App.js
@@ -18,30 +18,6 @@ function App() {
  
     );   
     
-    // const [data, setData] = useState([{}])
-    // useEffect(() => {
-    //     fetch("/members").then(
-    //         res => res.json()
-    //     ).then(
-    //         data => {
-    //             setData(data)
-    //             console.log(data)
-    //         }
-    //     )
-    // }, [])
-
-    // return (
-    //     <div>
-    //         {(typeof data.members === 'undefined') ? (
-    //             <p>Loading.....</p>
-    //         ) : (
-    //             data.members.map((member, i) => (
-    //                 <p key = {i}>{member}</p>
-
-    //             ))
-    //         )}
-    //     </div>
-    // )
 }
 
 export default App
