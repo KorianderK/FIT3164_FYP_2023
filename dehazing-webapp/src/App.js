829e38e25e8948a3961370716e175f311c5fc106
KorianderKAemulus <114248547+KorianderK@users.noreply.github.com>
2023-08-13 02:36:06 +0800

    Commit 5: Navbar, Title
    Added a NavBar, a Title and removed the choose image button after an image is uploaded.

--- dehazing-webapp/src/App.js
+++ dehazing-webapp/src/App.js
@@ -1,23 +1,55 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
+import { Icon, Button, Sidebar, Menu } from 'semantic-ui-react';
+import './App.css';
 
 function App() {
+    const [file, setFile] = useState(null);
 
-    const [file, setFile] = useState();
     function handleChange(e) {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
+        const selectedFile = e.target.files[0];
+
+        if (selectedFile) {
+            const allowedExtensions = ['jpg', 'jpeg', 'png'];
+            const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
+
+            if (allowedExtensions.includes(fileExtension)) {
+                setFile(URL.createObjectURL(selectedFile));
+            } else {
+                window.alert('Invalid file type. Please select an image file.');
+            }
+        }
     }
- 
+
+    function handleRemove() {
+        setFile(null);
+    }
+
     return (
         <div className="App">
-            <h2>Add Image:</h2>
-            <input type="file" onChange={handleChange} />
-            <img src={file} />
- 
+            <header className="App-header">
+                <h1 className="app-title">Image Dehazer V1</h1>
+            </header>
+            {!file ? (
+                <div>
+                    <label htmlFor="fileInput" className="upload-button-label">
+                        <Icon name="file" className="upload-button-icon" />
+                        Choose Image
+                    </label>
+                    <input id="fileInput" type="file" accept=".jpg, .jpeg, .png" onChange={handleChange} hidden />
+                </div>
+            ) : (
+                <div className="image-container">
+                    <p>Your Uploaded Image</p>
+                    <img src={file} alt="Uploaded" />
+                    <div></div>
+                    <Button className="remove-button" color="red" onClick={handleRemove}>
+                        Remove Upload
+                    </Button>
+                </div>
+            )}
         </div>
- 
-    );   
-    
+    );
 }
 
-export default App
+export default App;
+
