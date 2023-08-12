1834779b24fffd873502e8e618bc7884eba41a50
KorianderKAemulus <114248547+KorianderK@users.noreply.github.com>
2023-08-13 00:21:29 +0800

    Added image input button

--- dehazing-webapp/src/App.js
+++ dehazing-webapp/src/App.js
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from 'react'
 
 function App() {
-    
-    const [data, setData] = useState([{}])
-    useEffect(() => {
-        fetch("/members").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-                console.log(data)
-            }
-        )
-    }, [])
 
+    const [file, setFile] = useState();
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+ 
     return (
-        <div>
-            {(typeof data.members === 'undefined') ? (
-                <p>Loading.....</p>
-            ) : (
-                data.members.map((member, i) => (
-                    <p key = {i}>{member}</p>
-
-                ))
-            )}
+        <div className="App">
+            <h2>Add Image:</h2>
+            <input type="file" onChange={handleChange} />
+            <img src={file} />
+ 
         </div>
-    )
+ 
+    );   
+    
+    // const [data, setData] = useState([{}])
+    // useEffect(() => {
+    //     fetch("/members").then(
+    //         res => res.json()
+    //     ).then(
+    //         data => {
+    //             setData(data)
+    //             console.log(data)
+    //         }
+    //     )
+    // }, [])
+
+    // return (
+    //     <div>
+    //         {(typeof data.members === 'undefined') ? (
+    //             <p>Loading.....</p>
+    //         ) : (
+    //             data.members.map((member, i) => (
+    //                 <p key = {i}>{member}</p>
+
+    //             ))
+    //         )}
+    //     </div>
+    // )
 }
 
 export default App
