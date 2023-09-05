b5eb8684efdf11281258b0d85cba1a1e59eef5d8
Amos <John.Leong547@gmail.com>
2023-09-05 15:22:01 +0800

    Commit 5/9/2023
    Image can be sent to Flask backend to perform operations on it, and returned to UI for display.

--- dehazing-webapp/src/Imageupload.js
+++ dehazing-webapp/src/Imageupload.js
@@ -1,52 +1,95 @@
 import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 import './styles.css';
-
+// import axios from 'axios';
 
 function Imageupload() {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
+  const [grayedImage, setGrayedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData] = useState(new FormData()); // Initialize formData
 
-    function handleChange(e) {
-        const selectedFile = e.target.files[0];
+  const handleChange = (e) => {
+    const selectedFile = e.target.files[0];
 
-        if (selectedFile) {
-            const allowedExtensions = ['jpg', 'jpeg', 'png'];
-            const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
+    if (selectedFile) {
+      const allowedExtensions = ['jpg', 'jpeg', 'png'];
+      const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
 
-            if (allowedExtensions.includes(fileExtension)) {
-                setFile(URL.createObjectURL(selectedFile));
-            } else {
-                window.alert('Invalid file type. Please select an image file.');
-            }
-        }
+      if (allowedExtensions.includes(fileExtension)) {
+        setFile(URL.createObjectURL(selectedFile));
+        formData.set('image', selectedFile); // Update formData with the selected file
+      } else {
+        window.alert('Invalid file type. Please select an image file.');
+      }
     }
+  };
 
-    function handleRemove() {
-        setFile(null);
+  const handleRemove = () => {
+    setFile(null);
+    setGrayedImage(null);
+    formData.delete('image'); // Remove the image from formData when removing it from the UI
+  };
+
+  const handleProcessImage = async () => {
+    if (!file) {
+      console.error('No image selected.'); // Log an error message
+      return;
     }
+    setIsLoading(true);
 
-    return (
-        <div className="App">
-            {!file ? (
-                <div>
-                    <label htmlFor="fileInput" className="upload-button-label">
-                        <i  className="search icon" />
-                        Select Image
-                    </label>
-                    <input id="fileInput" type="file" accept=".jpg, .jpeg, .png" onChange={handleChange} hidden />
-                </div>
-            ) : (
-                <div className="image-container">
-                    <p>Your Uploaded Image</p>
-                    <img src={file} alt="Uploaded" />
-                    <div></div>
-                    <Button className="remove-button" color="red" onClick={handleRemove}>
-                        Remove Image
-                    </Button>
-                </div>
-            )}
+    try {
+      const response = await fetch('http://127.0.0.1:5000/api/process-image', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.status === 200) {
+        const result = await response.blob();
+        setGrayedImage(URL.createObjectURL(result));
+      } else {
+        console.error('Error processing the image.');
+      }
+    } catch (error) {
+      console.error('Error processing the image:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!file ? (
+        <div>
+          <label htmlFor="fileInput" className="upload-button-label">
+            <i className="search icon" />
+            Select Image
+          </label>
+          <input id="fileInput" type="file" name="image" accept=".jpg, .jpeg, .png" onChange={handleChange} hidden />
         </div>
-    );
+      ) : (
+        <div className="image-container">
+          <p>Your Uploaded Image</p>
+          <img src={file} alt="Uploaded" />
+          <div></div>
+          <Button className="remove-button" color="red" onClick={handleRemove}>
+            Remove Image
+          </Button>
+          <Button className="process-button" onClick={handleProcessImage} disabled={isLoading}>
+            {isLoading ? 'Processing...' : 'Process Image'}
+          </Button>
+        </div>
+      )}
+
+      {/* Display grayscale image if available */}
+      {grayedImage && (
+        <div className="image-container">
+          <p>Grayscale Image</p>
+          <img src={grayedImage} alt="Grayscale" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Imageupload;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,32 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+from flask_cors import CORS 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route("/members")
-def members():
-    return {"members": ["Aidan", "Amos", "Zafri", "Raveendran"]}
+@app.route('/api/process-image', methods=['POST'])
+def process_image():
+    if 'image' not in request.files:
+        return jsonify(message='No file part'), 400
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    image_file = request.files['image']
+
+    if image_file.filename == '':
+        return jsonify(message='No selected file'), 400
+
+    try:
+        image_np = np.fromfile(image_file, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        _, encoded_image = cv2.imencode('.jpg', gray_image)
+        response = encoded_image.tobytes()
+
+        return response, 200
+    except Exception as e:
+        return jsonify(message='Error processing the image'), 500
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
