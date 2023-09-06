28c0656a710a40b489d4df89535b91f8389b8d67
Amos <John.Leong547@gmail.com>
2023-09-06 11:53:52 +0800

    Commit 6/9/2023
    Added code to automatically resize the uploaded image on the ReactUI itself based on aspect ratio.

--- dehazing-webapp/src/Imageupload.js
+++ dehazing-webapp/src/Imageupload.js
@@ -4,9 +4,10 @@ import './styles.css';
 
 function Imageupload() {
   const [file, setFile] = useState(null);
-  const [grayedImage, setGrayedImage] = useState(null);
+  const [dehazedImage, setDehazedImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [formData] = useState(new FormData()); // Initialize formData
+  const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 });
 
   const handleChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -26,8 +27,9 @@ function Imageupload() {
 
   const handleRemove = () => {
     setFile(null);
-    setGrayedImage(null);
+    setDehazedImage(null);
     formData.delete('image'); // Remove the image from formData when removing it from the UI
+    setImageDimensions({ width: 0, height: 0 }); // Reset image dimensions
   };
 
   const handleProcessImage = async () => {
@@ -36,20 +38,20 @@ function Imageupload() {
       return;
     }
     setIsLoading(true);
-  
+
     try {
       const response = await fetch('http://127.0.0.1:5000/api/process-image', {
         method: 'POST',
         body: formData,
       });
-  
+
       if (response.status === 200) {
         const result = await response.blob();
         // Simulate a 1-second loading period
         setTimeout(() => {
-          setGrayedImage(URL.createObjectURL(result));
+          setDehazedImage(URL.createObjectURL(result));
           setIsLoading(false); // Turn off loading indicator after the delay
-        }, 1000); // 1000 milliseconds = 3 seconds
+        }, 200); // 1000 milliseconds = 3 seconds
       } else {
         console.error('Error processing the image.');
         setIsLoading(false); // Turn off loading indicator in case of an error
@@ -59,7 +61,39 @@ function Imageupload() {
       setIsLoading(false); // Turn off loading indicator in case of an error
     }
   };
+
+  // Function to calculate the aspect ratio
+  const calculateAspectRatio = (image) => {
+    return image.width / image.height;
+  };
+
+  const handleImageLoad = (e) => {
+    const image = e.target;
+    const aspectRatio = calculateAspectRatio(image);
   
+    // Now you have the aspectRatio value, which is width/height.
+    console.log('Aspect Ratio:', aspectRatio);
+  
+    // Define the target dimensions based on the aspect ratio
+    let targetWidth, targetHeight;
+  
+    if (aspectRatio >= 1.3 && aspectRatio <= 2.39) {
+      // Landscape aspect ratio range (1.3 to 2.39)
+      targetWidth = 720;
+      targetHeight = 480;
+    } else if (aspectRatio >= 1.0 && aspectRatio <= 1.3) {
+      // Portrait aspect ratio range (1.0 to 1.3)
+      targetWidth = 480;
+      targetHeight = 720;
+    } else {
+      // If the aspect ratio doesn't fall into the specified ranges, use defaults
+      targetWidth = 720; // Default landscape width
+      targetHeight = 480; // Default landscape height
+    }
+  
+    setImageDimensions({ width: targetWidth, height: targetHeight });
+  };
+
   return (
     <div className="App">
       {!file ? (
@@ -73,7 +107,16 @@ function Imageupload() {
       ) : (
         <div className="image-container fade-in">
           <p>Your Uploaded Image</p>
-          <img src={file} alt="Uploaded" className="fade-in responsive-image"/>
+          <img
+            src={file}
+            alt="Uploaded"
+            className="fade-in resized-image"
+            onLoad={handleImageLoad}
+            style={{
+              maxWidth: `${imageDimensions.width}px`,
+              maxHeight: `${imageDimensions.height}px`,
+            }}
+          />
           <div></div>
           <Button className="remove-button fade-in" color="red" onClick={handleRemove}>
             Remove Image
@@ -85,10 +128,14 @@ function Imageupload() {
       )}
 
       {/* Display grayscale image if available */}
-      {grayedImage && (
+      {dehazedImage && (
         <div className="image-container fade-in">
           <p>DCP + CLAHE Dehazed Image</p>
-          <img src={grayedImage} alt="Grayscale" className="fade-in responsive-image"/>
+          <img src={dehazedImage} alt="DCP_Processed" className="fade-in resized-image" 
+          style={{
+              maxWidth: `${imageDimensions.width}px`,
+              maxHeight: `${imageDimensions.height}px`,
+            }} />
         </div>
       )}
     </div>
