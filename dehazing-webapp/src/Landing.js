db7d9c18b1b04109bf7c5f7d58cd306af2b4f7da
Amos <John.Leong547@gmail.com>
2023-10-12 19:42:28 +0800

    Commit 3 12/10/2023
    Added background-image transitions to the Landing.js component.

--- dehazing-webapp/src/Landing.js
+++ dehazing-webapp/src/Landing.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.css';
 import Loading from './Loading';
 
+// Define an array of image URLs
+const images = [
+  'url(https://metro.co.uk/wp-content/uploads/2018/06/gettyimages-682966492.jpg?quality=90&strip=all)',
+  'url(https://wallpapercave.com/wp/wp4941878.jpg)',
+  'url(https://wallpapercave.com/wp/wp4941977.jpg)',
+  'url(https://wallpapercave.com/wp/wp4942008.jpg)',
+  'url(https://wallpapercave.com/wp/wp4942012.jpg)',
+  'url(https://wallpapercave.com/wp/wp4941976.jpg)',
+  'url(https://wallpapercave.com/wp/wp3291081.jpg)',
+  'url(https://wallpapercave.com/wp/wp4941929.jpg)'
+];
+
 function Landingpage() {
   const [showLoading, setShowLoading] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    // Move the changeBackgroundImage function inside the useEffect callback
+    const changeBackgroundImage = () => {
+      const newIndex = (currentImageIndex + 1) % images.length;
+      setCurrentImageIndex(newIndex);
+    };
+
+    const imageChangeInterval = setInterval(changeBackgroundImage, 7000);
+
+    return () => {
+      clearInterval(imageChangeInterval);
+    };
+  }, [currentImageIndex]);
 
   const handleGetStartedClick = () => {
     setShowLoading(true);
@@ -16,7 +43,7 @@ function Landingpage() {
 
   return (
     <div className="home-container fade-in">
-      <header className="hero">
+      <header className="hero" style={{ backgroundImage: images[currentImageIndex] }}>
         <h1 className="app-title">ClearView: Effective Dehazing Solutions</h1>
         <p className="app-description">Enhance your photos by removing haze and improving clarity, performance metrics included!</p>
         <a href="/userguide" onClick={handleGetStartedClick} className="cta-button">User Guide</a>
@@ -36,7 +63,7 @@ function Landingpage() {
         </div>
       </section>
       <footer className="footer">
-      <p>&copy; 2023 ClearView MDS-13. All rights reserved.</p>
+        <p>&copy; 2023 ClearView MDS-13. All rights reserved.</p>
       </footer>
 
       {showLoading && <Loading />}
